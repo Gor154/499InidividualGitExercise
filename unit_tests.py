752dd65fbcfdd23fecf00fed3ae017d1d03c09bb
Ilya Yereferenko <ilya@curex.org>
2022-09-18 23:39:27 +0600

    Add tests to confirm is_prime is working correctly

--- unit_tests.py
+++ unit_tests.py
@@ -5,6 +5,13 @@ def test_if_even():
     number = 25
     assert main_code.is_even(number) is False
 
+
+def test_if_prime():
+    number = 59
+    assert main_code.is_prime(number) is True
+    number = 222
+    assert main_code.is_prime(number) is False
+
 def test_output(capfd):
     number = 345
     main_code.break_up(number)
