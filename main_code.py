f41605df2f597b38de8f37cead5532b109d0a677
Ilya Yereferenko <ilya@curex.org>
2022-09-18 23:38:04 +0600

    Add logic to test if number is prime and prin the result

--- main_code.py
+++ main_code.py
@@ -1,3 +1,6 @@
+import math
+
+
 def break_up(number: int):
     print("Hundreds: " + str(number // 100 % 10) + " Tens: " + str(number // 10 % 10) + " Ones: " + str(number % 10))
     is_even(number)
@@ -12,6 +15,13 @@ def is_even(number: int):
         return False
 
 
+def is_prime(number: int):
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if (number % i) == 0:
+            return False
+    return True
+
+
 def main():
     number = 0
     while int(number) < 1 or int(number) > 999:
@@ -19,6 +29,10 @@ def main():
         if not number.isnumeric():
             number = 0
     break_up(int(number))
+    if is_prime(int(number)):
+        print(number+" is prime")
+    else:
+        print(number+" is not prime")
 
 
 if __name__ == "__main__":
